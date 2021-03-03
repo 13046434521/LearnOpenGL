77d9353629c18dee4f27fd4704206cdddb4a0fef
龙昊 <604872657@qq.com>
2021-03-03 18:42:26 +0800

    opengl first commit

--- LearnOpenGL/Source.cpp
+++ LearnOpenGL/Source.cpp
@@ -1,1 +1,93 @@
-#include <g>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+const unsigned int width = 800;
+const unsigned int height = 600;
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
+
+void initGL(int width, int height);
+void drawFrame(int width, int height);
+
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+
+	GLFWwindow* window = glfwCreateWindow(width, height,"LearnOpenGL",NULL,NULL);
+
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	initGL(width, height);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		processInput(window);
+
+		// 渲染逻辑代码
+		drawFrame(width, height);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+
+	return -1;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window){
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window, true);
+	}
+}
+
+GLuint vbo;
+struct Vertex
+{
+	float vertex[9] = {
+		-0.5f,-0.5f,1.0f,
+		0.5f,-0.5f,1.0f,
+		0.0f,0.5f,1.0f
+	};
+};
+
+void initGL(int width, int height) {
+	Vertex vertex;
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER,vbo);
+	glBufferData(GL_ARRAY_BUFFER,sizeof(vertex.vertex)*4, vertex.vertex,GL_STATIC_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+}
+
+void drawFrame(int width, int height) {
+	glClearColor(0.0f,0.0f,1.0f,1.0f);
+	glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+}
